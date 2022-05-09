20744fbd81df8f983e73c476be82093901be33c5
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-05-09 15:25:10 -0400

    commit tree c9d988193123bfbf7811b1aae983b72fdb8af6eb

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+class Cat{
+    constructor(name,sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {return `${this.name} says meow!`}
+}
+
+class Dog{
+    constructor(name,sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {return `${this.name} says woof!`}
+}
+
+class Bird{
+    constructor(name,sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        if(this.sex==='male') {
+            return `It\'s me!${this.name}, the Parrot!`
+        } else {
+        return `${this.name} says squawk!`}}
+}
+
+const softie = new Cat ('softie', 'female')
+const barkie = new Dog('barkie', 'male')
+const mosley = new Bird('mosley', 'female')
+const weasley = new Bird('weasley', 'male')
